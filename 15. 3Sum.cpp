6adb0cfa92f2d5ea979a2404018c74543bc3623b
JP <jeanpaulkhawam@protonmail.com>
2022-10-17 22:39:22 +0300

    uploaded solutions

---
+++ 15. 3Sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int> &nums) {
+        // sorting the array and fixing one pointer reduces the solution to the sorted 2sum problem
+        std::sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+
+        for (int i = 0; i < nums.size() - 2; i++) {
+            // dont have two combinations that start with the same number
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            size_t left = i + 1;
+            size_t right = nums.size() - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0) right--;
+                else if (sum < 0) left++;
+                else {
+                    res.push_back(std::vector<int>({nums[i], nums[left], nums[right]}));
+                    left++;
+                    // the left pointer shouldn't point to the same number twice
+                    while (nums[left] == nums[left - 1] && left < right) left++;
+                }
+            }
+        }
+        return res;
+    }
+};
