f99703996f625d57c6ac17a730dd5e007449e648
JP <jeanpaulkhawam@protonmail.com>
2022-10-31 14:54:17 +0200

    added solutions

--- 78. Subsets.cpp
+++ 78. Subsets.cpp
@@ -3,25 +3,23 @@
 using std::vector;
 
 class Solution {
-public:
-    vector<vector<int>> *output = new vector<vector<int>>();
-    int k;
+private:
+    vector<vector<int>> output;
 
-    void backtrack(int first, vector<int> &curr, vector<int> &nums) {
-        if (curr.size() == k) {
-            output->push_back(curr);
+    void backtrack(int index, vector<int> &curr, vector<int> &nums) {
+        if (index == nums.size()) {
+            output.push_back(curr);
             return;
         }
-        for (int i = first; i < nums.size(); i++) {
-            curr.push_back(nums[i]);
-            backtrack(i + 1, curr, nums);
-            curr.pop_back();
-        }
+        curr.push_back(nums[index]);
+        backtrack(index + 1, curr, nums);
+        curr.pop_back();
+        backtrack(index + 1, curr, nums);
     }
 
+public:
     vector<vector<int>> subsets(vector<int> &nums) {
-        for (k = 0; k <= nums.size(); k++)
-            backtrack(0, *new vector<int>(), nums);
-        return *output;
+        backtrack(0, *new vector<int>, nums);
+        return output;
     }
 };
--- 90. Subsets II.cpp
+++ 90. Subsets II.cpp
@@ -4,32 +4,25 @@
 using std::vector;
 
 class Solution {
-private:
-    vector<vector<int>> *output = new vector<vector<int>>();
+public:
+    vector<vector<int>> output;
 
     void backtrack(int index, vector<int> &curr, vector<int> &nums) {
         if (index == nums.size()) {
-            output->push_back(curr);
+            output.push_back(curr);
             return;
         }
-
-        // All subsets that include nums[index]
         curr.push_back(nums[index]);
         backtrack(index + 1, curr, nums);
         curr.pop_back();
-
-        // All subsets that don't include nums[index]
-        while (index + 1 < nums.size() && nums[index] == nums[index + 1])
-            // skip over any duplicates since they are next to each other
-            index++;
+        while (index + 1 < nums.size() && nums[index] == nums[index + 1]) index++;
         backtrack(index + 1, curr, nums);
     }
 
-public:
     vector<vector<int>> subsetsWithDup(vector<int> &nums) {
-        // sort input array to make possible skipping over duplicates
         std::sort(nums.begin(), nums.end());
-        backtrack(0, *new vector<int>(), nums);
-        return *output;
+        backtrack(0, *new vector<int>, nums);
+        return output;
     }
 };
+
