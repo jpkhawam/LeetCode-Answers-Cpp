60ddf7f73581a40740d69f371b674e43ca12ccfa
JP <jeanpaulkhawam@protonmail.com>
2022-11-14 17:59:21 +0200

    added solutions

--- 56. Merge Intervals.cpp
+++ 56. Merge Intervals.cpp
@@ -4,23 +4,22 @@
 using std::vector;
 
 class Solution {
-private:
-    static bool sortIntervalsComp(vector<int> &v1, vector<int> &v2) {
-        return v1[0] < v2[0];
-    }
-
 public:
     vector<vector<int>> merge(vector<vector<int>> &intervals) {
-        std::sort(intervals.begin(), intervals.end(), sortIntervalsComp);
-        for (int i = 0; i < intervals.size() - 1; i++) {
-            if (intervals[i][1] >= intervals[i + 1][0]) {
-                intervals[i][0] = std::min(intervals[i][0], intervals[i + 1][0]);
-                intervals[i][1] = std::max(intervals[i][1], intervals[i + 1][1]);
-                intervals.erase(intervals.begin() + i + 1);
-                i--;
-            }
+        if (intervals.size() <= 1) {
+            return intervals;
         }
-        return intervals;
+        std::sort(intervals.begin(), intervals.end());
+        vector<vector<int>> result;
+        result.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (result.back()[1] < intervals[i][0])
+                result.push_back(intervals[i]);
+            else
+                result.back()[1] = std::max(result.back()[1], intervals[i][1]);
+        }
+        return result;
     }
 };
 
